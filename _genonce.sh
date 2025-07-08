43b7861a03916a38b9f90ac82c2cf4f87b6cbea6
Dokotela <Dokotela@users.noreply.github.com>
2025-07-08 12:09:26 -0400

    auto generation for dart

--- _genonce.sh
+++ _genonce.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+# Function to convert JSON to Dart format
+json_to_dart() {
+    local input_file="$1"
+    local output_file="$2"
+    local resource_type="$3"
+    local variable_name="$4"
+    
+    # Read JSON and convert to Dart format
+    python3 -c "
+import json
+with open('$input_file', 'r') as f:
+    data = json.load(f)
+dart_json = json.dumps(data, indent=2)
+print(\"import 'package:fhir_r4/fhir_r4.dart' show $resource_type;\")
+print()
+print(f\"final $resource_type $variable_name = $resource_type.fromJson({dart_json});\")
+" > "$output_file"
+}
+
 # Run the IG Publisher directly
 java -jar publisher.jar -ig .
 
+# Copy JSON files to assets (existing functionality)
 cp output/CodeSystem-botswana-amr-local-method-cs.json flutter/assets/CodeSystem-botswana-amr-local-method-cs.json
 cp output/CodeSystem-botswana-amr-local-organism-cs.json flutter/assets/CodeSystem-botswana-amr-local-organism-cs.json
-cp output/CodeSystem-botswana-amr-local-test-cs.json flutter/assets/CodeSystem-botswana-amr-local-test-cs.json
 cp output/ValueSet-botswana-amr-antibiotic-susceptibility-loinc.json flutter/assets/ValueSet-botswana-amr-antibiotic-susceptibility-loinc.json
 cp output/ValueSet-botswana-amr-gram-stain-result-vs.json flutter/assets/ValueSet-botswana-amr-gram-stain-result-vs.json
 cp output/ValueSet-botswana-amr-identification-method-vs.json flutter/assets/ValueSet-botswana-amr-identification-method-vs.json
@@ -16,6 +35,18 @@ cp output/ValueSet-botswana-amr-specimen-site-vs.json flutter/assets/ValueSet-bo
 cp output/ValueSet-botswana-amr-specimen-type-vs.json flutter/assets/ValueSet-botswana-amr-specimen-type-vs.json
 cp output/ValueSet-botswana-amr-test-result-status-vs.json flutter/assets/ValueSet-botswana-amr-test-result-status-vs.json
 
-cd output
+# Convert to Dart files
+json_to_dart "output/CodeSystem-botswana-amr-local-method-cs.json" "flutter/lib/models/code_system-botswana-amr-local-method-cs.dart" "CodeSystem" "botswanaAmrLocalMethodCs"
+json_to_dart "output/CodeSystem-botswana-amr-local-organism-cs.json" "flutter/lib/models/code_system-botswana-amr-local-organism-cs.dart" "CodeSystem" "botswanaAmrLocalOrganismCs"
+json_to_dart "output/ValueSet-botswana-amr-antibiotic-susceptibility-loinc.json" "flutter/lib/models/value_set-botswana-amr-antibiotic-susceptibility-loinc.dart" "ValueSet" "botswanaAmrAntibioticSusceptibilityLoinc"
+json_to_dart "output/ValueSet-botswana-amr-gram-stain-result-vs.json" "flutter/lib/models/value_set-botswana-amr-gram-stain-result-vs.dart" "ValueSet" "botswanaAmrGramStainResultVs"
+json_to_dart "output/ValueSet-botswana-amr-identification-method-vs.json" "flutter/lib/models/value_set-botswana-amr-identification-method-vs.dart" "ValueSet" "botswanaAmrIdentificationMethodVs"
+json_to_dart "output/ValueSet-botswana-amr-interpretation.json" "flutter/lib/models/value_set-botswana-amr-interpretation.dart" "ValueSet" "botswanaAmrInterpretation"
+json_to_dart "output/ValueSet-botswana-amr-organism-vs.json" "flutter/lib/models/value_set-botswana-amr-organism-vs.dart" "ValueSet" "botswanaAmrOrganismVs"
+json_to_dart "output/ValueSet-botswana-amr-special-test-vs.json" "flutter/lib/models/value_set-botswana-amr-special-test-vs.dart" "ValueSet" "botswanaAmrSpecialTestVs"
+json_to_dart "output/ValueSet-botswana-amr-specimen-site-vs.json" "flutter/lib/models/value_set-botswana-amr-specimen-site-vs.dart" "ValueSet" "botswanaAmrSpecimenSiteVs"
+json_to_dart "output/ValueSet-botswana-amr-specimen-type-vs.json" "flutter/lib/models/value_set-botswana-amr-specimen-type-vs.dart" "ValueSet" "botswanaAmrSpecimenTypeVs"
+json_to_dart "output/ValueSet-botswana-amr-test-result-status-vs.json" "flutter/lib/models/value_set-botswana-amr-test-result-status-vs.dart" "ValueSet" "botswanaAmrTestResultStatusVs"
 
+cd output
 python3 -m http.server 8000
--- flutter/lib/models/value_set-botswana-amr-gram-stain-result-vs.dart
+++ flutter/lib/models/value_set-botswana-amr-gram-stain-result-vs.dart
@@ -1,26 +1,27 @@
 import 'package:fhir_r4/fhir_r4.dart' show ValueSet;
 
-final ValueSet botswanaAMRGramStainResultVS = ValueSet.fromJson({
+final ValueSet botswanaAmrGramStainResultVs = ValueSet.fromJson({
   "resourceType": "ValueSet",
   "id": "botswana-amr-gram-stain-result-vs",
   "text": {
     "status": "generated",
-    "div":
-        "<div xmlns=\"http://www.w3.org/1999/xhtml\"><p class=\"res-header-id\"><b>Generated Narrative: ValueSet botswana-amr-gram-stain-result-vs</b></p><a name=\"botswana-amr-gram-stain-result-vs\"> </a><a name=\"hcbotswana-amr-gram-stain-result-vs\"> </a><ul><li>Include these codes as defined in <a href=\"http://www.snomed.org/\"><code>http://snomed.info/sct</code></a><table class=\"none\"><tr><td style=\"white-space:nowrap\"><b>Code</b></td><td><b>Display</b></td></tr><tr><td><a href=\"http://snomed.info/id/8745002\">8745002</a></td><td>Gram-positive</td></tr><tr><td><a href=\"http://snomed.info/id/87172008\">87172008</a></td><td>Gram-negative</td></tr><tr><td><a href=\"http://snomed.info/id/411933009\">411933009</a></td><td>Gram-variable</td></tr><tr><td><a href=\"http://snomed.info/id/58296003\">58296003</a></td><td>Yeast</td></tr><tr><td><a href=\"http://snomed.info/id/74964007\">74964007</a></td><td>Other</td></tr></table></li></ul></div>"
+    "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\"><p class=\"res-header-id\"><b>Generated Narrative: ValueSet botswana-amr-gram-stain-result-vs</b></p><a name=\"botswana-amr-gram-stain-result-vs\"> </a><a name=\"hcbotswana-amr-gram-stain-result-vs\"> </a><ul><li>Include these codes as defined in <a href=\"http://www.snomed.org/\"><code>http://snomed.info/sct</code></a><table class=\"none\"><tr><td style=\"white-space:nowrap\"><b>Code</b></td><td><b>Display</b></td></tr><tr><td><a href=\"http://snomed.info/id/8745002\">8745002</a></td><td>Gram-positive</td></tr><tr><td><a href=\"http://snomed.info/id/87172008\">87172008</a></td><td>Gram-negative</td></tr><tr><td><a href=\"http://snomed.info/id/411933009\">411933009</a></td><td>Gram-variable</td></tr><tr><td><a href=\"http://snomed.info/id/58296003\">58296003</a></td><td>Yeast</td></tr><tr><td><a href=\"http://snomed.info/id/74964007\">74964007</a></td><td>Other</td></tr></table></li></ul></div>"
   },
-  "url":
-      "http://bw.health.gov/fhir/ImplementationGuide/bw-amr-ig/ValueSet/botswana-amr-gram-stain-result-vs",
+  "url": "http://bw.health.gov/fhir/ImplementationGuide/bw-amr-ig/ValueSet/botswana-amr-gram-stain-result-vs",
   "version": "0.1.0",
   "name": "BotswanaAMRGramStainResultVS",
   "title": "Botswana AMR Gram Stain Results",
   "status": "draft",
-  "date": "2025-06-25T09:59:52-04:00",
+  "date": "2025-07-08T12:05:30-04:00",
   "publisher": "University of Botswana Partnership",
   "contact": [
     {
       "name": "University of Botswana Partnership",
       "telecom": [
-        {"system": "url", "value": "https://github.com/Dokotela/bw-amr-ig"}
+        {
+          "system": "url",
+          "value": "https://github.com/Dokotela/bw-amr-ig"
+        }
       ]
     }
   ],
@@ -30,11 +31,26 @@ final ValueSet botswanaAMRGramStainResultVS = ValueSet.fromJson({
       {
         "system": "http://snomed.info/sct",
         "concept": [
-          {"code": "8745002", "display": "Gram-positive"},
-          {"code": "87172008", "display": "Gram-negative"},
-          {"code": "411933009", "display": "Gram-variable"},
-          {"code": "58296003", "display": "Yeast"},
-          {"code": "74964007", "display": "Other"}
+          {
+            "code": "8745002",
+            "display": "Gram-positive"
+          },
+          {
+            "code": "87172008",
+            "display": "Gram-negative"
+          },
+          {
+            "code": "411933009",
+            "display": "Gram-variable"
+          },
+          {
+            "code": "58296003",
+            "display": "Yeast"
+          },
+          {
+            "code": "74964007",
+            "display": "Other"
+          }
         ]
       }
     ]
